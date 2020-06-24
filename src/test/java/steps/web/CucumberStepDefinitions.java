f0b1b58a507a70c3651e96578c61759393997ad8
aroygas <a.roygas@gmail.com>
2020-06-24 17:51:28 +0300

    Added first 6 described scenarios

--- src/test/java/steps/web/CucumberStepDefinitions.java
+++ src/test/java/steps/web/CucumberStepDefinitions.java
@@ -1,15 +1,23 @@
 package steps.web;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import static com.codeborne.selenide.Condition.appears;
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,6 +25,8 @@ import static com.codeborne.selenide.Selenide.*;
  * All Cucumber steps are implemented in this class.
  */
 public class CucumberStepDefinitions {
+    String randomCityName;
+
     /**
      * Cucumber "Before" hook that is executed before each suite run.
      * Set's up browser for Selenide.
@@ -61,5 +71,74 @@ public class CucumberStepDefinitions {
     @Then("I should see \"([^\"]*)\" page title")
     public void iShouldSeePageTitle(String pageTitle) {
         $("title").shouldHave(attribute("text", pageTitle));
+    }
+
+    @And("I open city choice menu")
+    public void iOpenCityChoiceMenu() {
+        String menuXpath = "//div[contains(@class, 'region')]//a[contains(@class, 'chooseRepresent')]";
+
+        $(By.xpath(menuXpath)).click();
+    }
+
+    @Then("I should see \"([^\"]*)\" text")
+    public void iShouldSeeText(String text) {
+        String textXpath = "//*[contains(., \"" + text + "\")]";
+
+        $(By.xpath(textXpath)).shouldBe(appears);
+    }
+
+    @And("I add item to shopping cart by sale price")
+    public void iAddItemToShoppingCartBySalePrice() {
+        String buttonXpath = "//div[@id=\"goods-analog\"]//a[contains(@data-behavior, \"add-to-cart\")]";
+
+        $(By.xpath(buttonXpath)).click();
+    }
+
+    @And("I choose random city with delivery")
+    public void iChooseDeliveryToRandomCity() {
+        String citiesXpath = "//div[contains(@class, 'deliveryCourierConteiner')]//a[not(contains(@class, 'fs-14 fst-b'))]";
+        int index;
+
+        int numberOfCities = $$(By.xpath(citiesXpath)).size();
+        if (numberOfCities>1) {
+            index = (new Random()).nextInt(numberOfCities - 1);
+        } else {
+            index = 1;
+        }
+
+        SelenideElement randomCity = $(By.xpath(citiesXpath), index);
+        randomCityName = randomCity.getText();
+        randomCity.click();
+    }
+
+    @Then("I should see random city chosen")
+    public void iShouldSeeCityChosen() {
+        String actualCityXpath = "//div[contains(@class, 'region')]//a[contains(@class, 'chooseRepresent')]/span";
+
+        String actualCityName = $(By.xpath(actualCityXpath)).getText();
+
+        Assert.assertEquals(actualCityName, randomCityName);
+    }
+
+    @And("I choose random item with best price")
+    public void iChooseRandomItemWithBestPrice() {
+        String bestPriceItemsXpath = "//div[contains(@class, 'tile-box product') and .//a[contains(., 'Лучшая цена')]]//a[contains(., 'В корзину')]";
+        int index;
+
+        int numberOfItems = $$(By.xpath(bestPriceItemsXpath)).size();
+        if (numberOfItems>1) {
+            index = (new Random()).nextInt(numberOfItems - 1);
+        } else {
+            index = 1;
+        }
+
+        $(By.xpath(bestPriceItemsXpath), index).click();
+    }
+
+    @Then("I should see \"([^\"]*)\" phone number for \"([^\"]*)\" department")
+    public void iShouldSeePhoneNumberForDepartment(String phoneNumber, String departmentName) {
+        String phoneXpath = "//tr[contains(@class, 'contact_page_table_line') and .//td[contains(., '" + departmentName + "')]]//td[contains(@class, 'contact_info_table_telefon')]";
+
+        Assert.assertEquals($(By.xpath(phoneXpath)).getText(), phoneNumber);
     }
 }
--- src/test/resources/features/vseinstrumenti.feature
+++ src/test/resources/features/vseinstrumenti.feature
@@ -4,6 +4,36 @@ Feature: Web store functionality
   I need to be able to search and buy goods on the web site
 
   @smoke
-  Scenario: Check web site tab header
+  Scenario: 1) Main page title
     When I open "http://spb.vseinstrumenti.ru" url
     Then I should see "Интернет-магазин ВсеИнструменты.ру в Санкт-Петербурге - электроинструмент, садовый и ручной инструмент, оборудование и станки" page title
+
+  @smoke
+  Scenario: 2) City choice menu check
+    When I open "http://spb.vseinstrumenti.ru" url
+    And I open city choice menu
+    Then I should see "Выберите свой город" text
+
+  @smoke
+  Scenario: 3) Sale price shopping
+    When I open "https://www.vseinstrumenti.ru/stanki/sverlilnye/stalex/sverlilnyj-stanok-stalex-sdi-25t-industrial-z4125b1" url
+    And I add item to shopping cart by sale price
+    Then I should see "Товар добавлен в корзину" text
+
+  @smoke
+  Scenario: 4) Random city with delivery choice
+    When I open "http://spb.vseinstrumenti.ru" url
+    And I open city choice menu
+    And I choose random city with delivery
+    Then I should see random city chosen
+
+  @smoke
+  Scenario: 5) Random best price item choice
+    When I open "https://www.vseinstrumenti.ru/instrument/gravery/pryamoshlifovalnye-mashiny" url
+    And I choose random item with best price
+    Then I should see "Товар добавлен в корзину" text
+
+  @smoke
+  Scenario: 6) Contacts number check
+    When I open "https://www.vseinstrumenti.ru/contacts/1.html" url
+    Then I should see "+7 (495) 647-60-00" phone number for "Отдел подбора персонала" department
